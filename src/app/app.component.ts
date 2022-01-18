305d68a177cae2aaf1d20d82194792cf296b6429
patilrushikesh247@gmail.com <58651016+patilrushikesh247@users.noreply.github.com>
2022-01-18 13:18:12 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'color-change';
+ 
+
+
+show="Show";
+  showMe = false;
+
+
+  toogleTag() {
+
+    this.showMe = !this.showMe
+    this.show = this.showMe ? 'show' : 'hide';
+  
+  }
+
+
+  toggle = true;
+  status = 'green';
+
+
+  buttonColor() {
+    this.toggle = !this.toggle;
+    this.status = this.toggle ? 'green' : 'red';
+  }
+
+
+  setRedClass: boolean = false;
+
+  toggleClass() {
+    this.setRedClass = !this.setRedClass;
+  }
+
+
+  firstName="Rushi";
+  green="green";
+  red="red";
+
+  userName="Rushikesh";
+   userSurname="patil";
+    age=19;
+
+  
+}
+
+
 
 
